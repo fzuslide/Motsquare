788b09be5d902fc7f9da20d8b629f887705a49db
fzuslide <fzuslide@gmail.com>
2012-10-18 09:31:05 +0800

    add demo1

--- Motsquare/urls.py
+++ Motsquare/urls.py
@@ -1,4 +1,10 @@
+#coding=utf-8
+
+import os
+
+from django.conf.urls.defaults import *
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 from Motsquare.views import square
 # Uncomment the next two lines to enable the admin:
@@ -6,14 +12,21 @@ from Motsquare.views import square
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'square.html', square.square_base),
-    url(r'^$', square.square_base ),
-    # url(r'^Motsquare/', include('Motsquare.foo.urls')),
+        # Examples:
+        url(r'^$', square.square_list ),
+        url(r'square_list.html', square.square_list),
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': os.path.join(settings.PROJECT_PATH, 'media')}),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+        # url(r'^Motsquare/', include('Motsquare.foo.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
+        # Uncomment the admin/doc line below to enable admin documentation:
+        # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+        # Uncomment the next line to enable the admin:
+        # url(r'^admin/', include(admin.site.urls)),
+
+        (r'^demo1.html', square.demo1),
+        )
+
+
--- Motsquare/views/square.py
+++ Motsquare/views/square.py
@@ -2,7 +2,13 @@
 
 
 from django.shortcuts import render_to_response
+from django.conf import settings
 
 
-def square_base(requset):
-    return render_to_response('square.html')
+def square_list(requset):
+    return render_to_response('square_list.html')
+
+
+def demo1(request):
+    return render_to_response('demo1.html',{"DEMO1_SERVER_PORT": settings.DEMO1_SERVER_PORT})
+
